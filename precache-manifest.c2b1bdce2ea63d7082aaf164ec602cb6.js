a669f547790a668378a46039641e7231a3306682
Aiya Siddig <aiya.siddig23@gmail.com>
2020-11-28 18:22:25 -0800

    Updates

---
+++ precache-manifest.c2b1bdce2ea63d7082aaf164ec602cb6.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7b44befad5d1aa25f96364dab32c2f1f",
+    "url": "/employee-directory/index.html"
+  },
+  {
+    "revision": "2aa0f9df182adec3af7d",
+    "url": "/employee-directory/static/css/2.437d98bc.chunk.css"
+  },
+  {
+    "revision": "a56182436332d71cb533",
+    "url": "/employee-directory/static/css/main.bbafc0f2.chunk.css"
+  },
+  {
+    "revision": "2aa0f9df182adec3af7d",
+    "url": "/employee-directory/static/js/2.e288cfb6.chunk.js"
+  },
+  {
+    "revision": "a56182436332d71cb533",
+    "url": "/employee-directory/static/js/main.a559c624.chunk.js"
+  },
+  {
+    "revision": "77a50e16a26474c82f8d",
+    "url": "/employee-directory/static/js/runtime~main.abed8607.js"
+  }
+]);
